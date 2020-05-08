39c46e583a1ebc64a9fe84fecc7db3164ad5853a
OuterCloud <outercloudstudio@gmail.com>
2020-05-08 15:52:22 -0500

    Update src.py

--- src.py
+++ src.py
@@ -1,7 +1,11 @@
 import numpy
 from random import seed
 from random import random
+import noise
 import math
+#             0   1   2   3   4   5   6   7   8   9  10
+numpy.array([" ",".","-","~","o","O","G","%","&","@","|"])
+
 #seed to prevent any errors that might happen
 #if seed is the same them random numbers will be the same
 #maybe use something to pick a seed maybe based on time?
@@ -15,3 +19,7 @@ def SetSeed(s):
 #get a random int within a range
 def RandRange(min,max):
     return (math.floor(random()*(max-min)+min))
+
+def TestNois():
+    pass
+

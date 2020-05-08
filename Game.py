ac41b63f652487ef97a5b5ed0b2bf0a5ac149501
spikerton3000 <spikerton3000@gmail.com>
2020-05-08 16:03:54 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,10 +1,23 @@
-import src
+from src import *
+
+def cockpit():
+    print("You open the door and go out.")
 
 def room1():
-    room = input("You wake up in a dark room. It is filled with the sound of screaming.")
+    print("You wake up to the sound of screaming in a dark room.")
+    input()
+    print("After careful consideration, you decide that it is not screaming, it is an engine.")
+    input()
+    door = input("You do not know how you got here, nor do you know where you are. But one thing that you do know is that there is a door in front of you. Do you open it and go out? (y or n)")
+    if door == "n":
+        print("You go back to sleep.")
+        input()
+        room1()
+    if door == "y":
+        cockpit()    
 
 def preparation():    
-    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again. Write 'continue' to keep going.")
+    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again. After I say a statement, click Enter. Write 'continue' to keep going.")
     if prep == "q":
         print("Goodbye")
     elif prep == "h":

0500ad14bf39060114c398dc44fa70fa369327a1
spikerton3000 <spikerton3000@gmail.com>
2020-05-08 16:16:38 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,11 @@
 from src import *
 
-def cockpit():
-    print("You open the door and go out.")
+def cockpit(name):
+    print("You open the door and go out. It seems that you have walked into a tangle of arms. legs, and voices.")
+    input()
 
-def room1():
+
+def room1(name):
     print("You wake up to the sound of screaming in a dark room.")
     input()
     print("After careful consideration, you decide that it is not screaming, it is an engine.")
@@ -12,17 +14,26 @@ def room1():
     if door == "n":
         print("You go back to sleep.")
         input()
-        room1()
+        room1(name)
     if door == "y":
-        cockpit()    
-
-def preparation():    
-    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again. After I say a statement, click Enter. Write 'continue' to keep going.")
-    if prep == "q":
-        print("Goodbye")
-    elif prep == "h":
-        preparation()
+        cockpit(name)
     else:
-        room1()
+        print(f"Please write one of the given answers, {name}.")
+        input()
+        room1(name)        
 
-preparation()
+def preparation(name):    
+    prep = input(f"Hello, {name.title()}. Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again. After I say a statement without asking you to answer, click Enter. Write 'continue' to keep going.")
+    if prep.lower() == "q":
+        print("Goodbye")
+    elif prep.lower() == "h":
+        preparation(name)
+    elif prep.lower() == "continue"
+        room1(name)
+    else:
+        print("Please write one of the given answers.")
+        input()
+        preparation(name)    
+
+name = input("What is your name?")
+preparation(name)

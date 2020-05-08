49d78d6f563041f741d2058f66a130e045b9e01e
spikerton3000 <spikerton3000@gmail.com>
2020-05-08 15:53:12 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,15 @@
 import src
+
+def room1():
+    room = input("You wake up in a dark room. It is filled with the sound of screaming.")
+
 def preparation():    
-    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again.")
+    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again. Write 'continue' to keep going.")
     if prep == "q":
         print("Goodbye")
-    if prep == "h":
-        preparation()    
+    elif prep == "h":
+        preparation()
+    else:
+        room1()
 
 preparation()

61441e64d860f42a394038af380484a50e4cafe1
spikerton3000 <spikerton3000@gmail.com>
2020-05-08 15:43:21 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,9 @@
 import src
+def preparation():    
+    prep = input("Here are the game rules: At any time, press 'q' to quit the game, or 'h' to read this again.")
+    if prep == "q":
+        print("Goodbye")
+    if prep == "h":
+        preparation()    
+
+preparation()

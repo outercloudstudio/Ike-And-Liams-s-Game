9057ff09f7eb99abff1900c2c43721aa658ef360
spikerton3000 <spikerton3000@gmail.com>
2020-05-07 17:05:33 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 import random
 <<<<<<< HEAD
 print("Hey Liam!")
 =======
+=======
+<<<<<<< HEAD
+import random
+
+Hey Ike
+=======
+>>>>>>> parent of bb7bf8f... Hey Liam
+>>>>>>> parent of 02e2549... h

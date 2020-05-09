04ff9dc2cab56784275cce6a7c8c50f59689c58d
OuterCloud <outercloudstudio@gmail.com>
2020-05-08 22:10:57 -0500

    Added generate race name
    allows you to generate the name of something in general I just named it race because it sounds like species.

--- src.py
+++ src.py
@@ -5,6 +5,11 @@ import math
 planetNames = ["Croid","Hyrusk","Atlasks","Liompa","Geh","Retd","Kring"]
 planetNames2= ["223","w220","sdhu","sdh2o","1e9dj","2d89","1hdk","dsfkh","sndi"]
 planetNamesCreated = []
+RaceName1 = ["R","T","Ch","W","C","Z","L"]
+RaceName2=["a","o","i","e","o"]
+RaceName3 =["ch","ts","ls","ps","sh","x","d","kr"]
+RaceName4 = ["","ex","s","y"]
+RacesCreated = []
 #seed to prevent any errors that might happen
 #if seed is the same them random numbers will be the same
 #maybe use something to pick a seed maybe based on time?
@@ -29,6 +34,22 @@ def GeneratePlanet() :
         if name ==t:
             GeneratePlanet()
     planetNamesCreated.append(name)
+    return name
+
+def GenerateRaceName():
+    name = ""
+    name +=RaceName1[RandRange(0,len(RaceName1)-1)]
+    name +=RaceName2[RandRange(0,len(RaceName2)-1)]
+    name +=RaceName3[RandRange(0,len(RaceName3)-1)]
+    name +=RaceName4[RandRange(0,len(RaceName4)-1)]
+    for i in RacesCreated:
+        if name == i:
+            name = "Race "
+            name += str(RandRange(100000,999999))
+    for t in RacesCreated:
+        if name ==t:
+            GenerateRaceName()
+    RacesCreated.append(name)
     return name
 
 
